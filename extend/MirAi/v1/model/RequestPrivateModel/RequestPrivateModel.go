8342bd5c0330c9316df586d8fc6c4deb4c634062
TobyCroft <silverhawk@foxmail.com>
2020-06-08 22:57:30 +0800

    Tuuz

---
+++ extend/MirAi/v1/model/RequestPrivateModel/RequestPrivateModel.go
@@ -0,0 +1,28 @@
+package RequestPrivateModel
+
+import (
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "request_private"
+
+func Api_insert(qq, eventId, message, fromId, groupId, nick interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"qq":      qq,
+		"eventId": eventId,
+		"message": message,
+		"fromId":  fromId,
+		"groupId": groupId,
+		"nick":    nick,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

a8ae75387877365d92efa47b291c4fa82a349a95
TobyCroft <silverhawk@foxmail.com>
2020-06-08 23:23:13 +0800

    Tuuz

---
+++ app/http/v1/model/LogUnknowModel/LogUnknowModel.go
@@ -0,0 +1,24 @@
+package LogUnknowModel
+
+import (
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "log_unknow"
+
+func Api_insert(qq, log interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"qq":  qq,
+		"log": log,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

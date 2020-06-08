ac5ad6459086991434b54386b636910965cadfad
TobyCroft <silverhawk@foxmail.com>
2020-06-08 22:29:41 +0800

    Tuuz

--- app/http/v1/action/MirRoute/MirRoute.go
+++ app/http/v1/action/MirRoute/MirRoute.go
@@ -1,8 +1,24 @@
 package MirRoute
 
-import "fmt"
+import (
+	"fmt"
+	"main.go/app/http/v1/model/LogModel"
+)
 
 func Do(qq, json string) {
 	fmt.Println(qq)
 	fmt.Println(json)
+	LogModel.Api_insert(qq, json)
+}
+
+func Message(qq, json string) {
+
+}
+
+func Notice(qq, json string) {
+
+}
+
+func Request(qq, json string) {
+
 }

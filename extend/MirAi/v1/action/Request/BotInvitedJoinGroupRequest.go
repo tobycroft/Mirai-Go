9dd355ec363b5f8f74531baaface1f112288ca53
Tuuz <2539@aerofsx.com>
2020-06-10 17:13:34 +0800

    Tuuz

--- extend/MirAi/v1/action/Request/BotInvitedJoinGroupRequest.go
+++ extend/MirAi/v1/action/Request/BotInvitedJoinGroupRequest.go
@@ -3,5 +3,5 @@ package Request
 import "main.go/extend/MirAi/v1/model/GroupRequestModel"
 
 func BotInvitedJoinGroupRequest(qq string, json map[string]interface{}) {
-	go GroupRequestModel.Api_insert(qq, json["eventId"], json["message"], json["fromId"], json["groupId"], json["groupName"], json["nick"])
+	GroupRequestModel.Api_insert(qq, json["eventId"], json["message"], json["fromId"], json["groupId"], json["groupName"], json["nick"])
 }

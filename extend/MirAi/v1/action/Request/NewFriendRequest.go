2b5d53edcbbc9268a811b4ccf9b8ded522c09e60
Tuuz <2539@aerofsx.com>
2020-06-10 17:13:38 +0800

    Tuuz

--- extend/MirAi/v1/action/Request/NewFriendRequest.go
+++ extend/MirAi/v1/action/Request/NewFriendRequest.go
@@ -3,5 +3,5 @@ package Request
 import "main.go/extend/MirAi/v1/model/PrivateRequestModel"
 
 func NewFriendRequest(qq string, json map[string]interface{}) {
-	go PrivateRequestModel.Api_insert(qq, json["eventId"], json["message"], json["fromId"], json["groupId"], json["nick"])
+	PrivateRequestModel.Api_insert(qq, json["eventId"], json["message"], json["fromId"], json["groupId"], json["nick"])
 }

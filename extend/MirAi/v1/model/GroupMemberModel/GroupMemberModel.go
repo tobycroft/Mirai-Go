28cb30e7b56b8ad86c79777d8c4afb162f9c51b2
Tuuz <2539@aerofsx.com>
2020-06-11 11:22:22 +0800

    Tuuz

---
+++ extend/MirAi/v1/model/GroupMemberModel/GroupMemberModel.go
@@ -0,0 +1,90 @@
+package GroupMemberModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "group_member"
+
+func Api_insert(qq, group_id, user_id, nickname, role interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"qq":       qq,
+		"group_id": group_id,
+		"user_id":  user_id,
+		"nickname": nickname,
+		"role":     role,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(group_id, user_id interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select(group_id interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+	}
+	db.Where(where)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_delete(group_id, user_id interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Where(where)
+	_, err := db.Delete()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_delete_group(group_id interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+	}
+	db.Where(where)
+	_, err := db.Delete()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

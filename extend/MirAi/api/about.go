6136b1a71ec666f825943e464cb540eacc024e36
Tuuz <2539@aerofsx.com>
2020-06-17 17:33:58 +0800

    Tuuz

--- extend/MirAi/api/about.go
+++ extend/MirAi/api/about.go
@@ -39,3 +39,39 @@ func Auth(qq interface{}) (map[string]interface{}, error) {
 	}
 	return nil, err
 }
+
+func Verify(qq, sessionKey interface{}) (map[string]interface{}, error) {
+	bot, ok := Bot.BotSingle(qq)
+	if !ok {
+		return nil, errors.New("未找到账号，可能机器人已经过期")
+	}
+	post := map[string]interface{}{
+		"qq":         qq,
+		"sessionKey": sessionKey,
+	}
+	ret, err := Net.Postraw(bot.URL+"/verify", nil, post, nil, nil)
+	if err != nil {
+		Log.Crrs(err, tuuz.FUNCTION_ALL())
+	} else {
+		return Jsong.JObject(ret)
+	}
+	return nil, err
+}
+
+func Release(qq, sessionKey interface{}) (map[string]interface{}, error) {
+	bot, ok := Bot.BotSingle(qq)
+	if !ok {
+		return nil, errors.New("未找到账号，可能机器人已经过期")
+	}
+	post := map[string]interface{}{
+		"qq":         qq,
+		"sessionKey": sessionKey,
+	}
+	ret, err := Net.Postraw(bot.URL+"/release", nil, post, nil, nil)
+	if err != nil {
+		Log.Crrs(err, tuuz.FUNCTION_ALL())
+	} else {
+		return Jsong.JObject(ret)
+	}
+	return nil, err
+}

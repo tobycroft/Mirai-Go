a01a907db17d07071756061d2c541d59ffe68450
Tuuz <2539@aerofsx.com>
2020-06-17 16:28:31 +0800

    Tuuz

--- extend/MirAi/api/about.go
+++ extend/MirAi/api/about.go
@@ -1,10 +1,28 @@
 package api
 
 import (
-	"main.go/config"
+	"main.go/extend/MirAi/v1/action/Bot"
+	"main.go/tuuz"
+	"main.go/tuuz/Jsong"
+	"main.go/tuuz/Log"
 	"main.go/tuuz/Net"
 )
 
-func About() {
-	ret, err := Net.Get(config.API_URL+"/about", nil, nil, nil)
+func About(qq string) (map[string]interface{}, error) {
+	bot := Bot.BotSingle(qq)
+	ret, err := Net.Get(bot.URL+"/about", nil, nil, nil)
+	if err != nil {
+		Log.Crrs(err, tuuz.FUNCTION_ALL())
+	} else {
+		return Jsong.ParseObject(ret)
+	}
+	return nil, err
+}
+
+func Auth(qq string) (map[string]interface{}, error) {
+	bot := Bot.BotSingle(qq)
+	post := map[string]interface{}{
+		"authKey": bot.AuthKey,
+	}
+	Net.Postraw(bot.URL+"/auth", nil, post, nil, nil)
 }

f2203907437c13f435a412259a70c4f8650b3e54
Tuuz <2539@aerofsx.com>
2020-06-17 17:43:54 +0800

    Tuuz

--- extend/MirAi/v1/action/Bot/Auth.go
+++ extend/MirAi/v1/action/Bot/Auth.go
@@ -1,31 +1,25 @@
 package Bot
 
-import (
-	"errors"
-	"main.go/extend/MirAi/api"
-	"main.go/extend/MirAi/v1/model/BotModel"
-)
-
-func App_Auth(qq interface{}) error {
-	ret, err := api.Auth(qq)
-	if err != nil {
-		return err
-	} else {
-		if ret["code"].(float64) == 0 {
-			session := ret["session"].(string)
-			//bind
-			ret, err = api.Verify(qq, session)
-			if err != nil {
-				return err
-			} else {
-				if BotModel.Api_update(qq, session) {
-					return nil
-				} else {
-					return errors.New("数据库插入失败")
-				}
-			}
-		} else {
-			return errors.New("获取session失败")
-		}
-	}
-}
+//func App_Auth(qq interface{}) error {
+//	ret, err := api.Auth(qq)
+//	if err != nil {
+//		return err
+//	} else {
+//		if ret["code"].(float64) == 0 {
+//			session := ret["session"].(string)
+//			//bind
+//			ret, err = api.Verify(qq, session)
+//			if err != nil {
+//				return err
+//			} else {
+//				if BotModel.Api_update(qq, session) {
+//					return nil
+//				} else {
+//					return errors.New("数据库插入失败")
+//				}
+//			}
+//		} else {
+//			return errors.New("获取session失败")
+//		}
+//	}
+//}

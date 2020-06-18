7b12be6a3cdf5e63dbe6294ac4960eccb0c3273d
Tuuz <2539@aerofsx.com>
2020-06-18 16:50:21 +0800

    Tuuz

--- extend/MirAi/cron/refresh_session.go
+++ extend/MirAi/cron/refresh_session.go
@@ -1,12 +1,9 @@
 package cron
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"main.go/extend/MirAi/api"
 	"main.go/extend/MirAi/v1/action/Bot"
-	"main.go/extend/MirAi/v1/model/BotModel"
 	"main.go/tuuz"
 	"main.go/tuuz/Log"
 	"time"
@@ -26,26 +23,9 @@ func refresh() {
 	} else {
 		for _, bot := range bots {
 			qq := bot.QQ
-			ret, err := api.Auth(qq)
+			err := api.App_Auth(qq)
 			if err != nil {
 				Log.Crrs(err, tuuz.FUNCTION_ALL())
-			} else {
-				if ret["code"].(float64) == 0 {
-					session := ret["session"].(string)
-					//bind
-					ret, err = api.Verify(qq, session)
-					if err != nil {
-						Log.Crrs(err, tuuz.FUNCTION_ALL())
-					} else {
-						if BotModel.Api_update(qq, session) {
-							fmt.Println(qq, "bot更新成功", session)
-						} else {
-							Log.Crrs(errors.New("bot_session更新失败,500"), tuuz.FUNCTION_ALL())
-						}
-					}
-				} else {
-					Log.Crrs(errors.New("获取session失败"), tuuz.FUNCTION_ALL())
-				}
 			}
 		}
 	}

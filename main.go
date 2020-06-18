f6db5747903c120315f4de819cb8697cb42f68da
Tuuz <2539@aerofsx.com>
2020-06-18 16:50:34 +0800

    Tuuz

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/extend/MirAi/cron"
 	"main.go/route"
 )
 
 func main() {
+	go cron.Refresh_session()
+
 	mainroute := gin.Default()
 	//gin.SetMode(gin.ReleaseMode)
 	//gin.DefaultWriter = ioutil.Discard

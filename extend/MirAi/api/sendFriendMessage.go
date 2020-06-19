ec43542b155ba4f3d02c784765bb0bbafd209493
Tuuz <2539@aerofsx.com>
2020-06-19 15:30:39 +0800

    Tuuz

--- extend/MirAi/api/sendFriendMessage.go
+++ extend/MirAi/api/sendFriendMessage.go
@@ -1,5 +1,29 @@
 package api
 
-func SendFriendMessage(qq, target interface{}) {
+import (
+	"main.go/extend/MirAi/v1/action/Bot"
+	"main.go/tuuz"
+	"main.go/tuuz/Jsong"
+	"main.go/tuuz/Log"
+	"main.go/tuuz/Net"
+)
 
+func SendFriendMessage(qq, target interface{}, messages []interface{}) (map[string]interface{}, error) {
+	bot, err := Bot.BotSingle(qq)
+	if err != nil {
+		return nil, err
+	} else {
+		post := map[string]interface{}{
+			"sessionKey":   bot.SessionKey,
+			"target":       target,
+			"messageChain": messages,
+		}
+		ret, err := Net.Postraw(bot.URL+"/sendFriendMessage", nil, post, nil, nil)
+		if err != nil {
+			Log.Crrs(err, tuuz.FUNCTION_ALL())
+			return nil, err
+		} else {
+			return Jsong.JObject(ret)
+		}
+	}
 }

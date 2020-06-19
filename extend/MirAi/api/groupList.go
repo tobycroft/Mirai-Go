af0004f8ccb37fdd7754539e9cc9c1c6c6a17d55
Tuuz <2539@aerofsx.com>
2020-06-19 15:58:09 +0800

    Tuuz

---
+++ extend/MirAi/api/groupList.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	"main.go/extend/MirAi/v1/action/Bot"
+	"main.go/tuuz"
+	"main.go/tuuz/Jsong"
+	"main.go/tuuz/Log"
+	"main.go/tuuz/Net"
+)
+
+func GroupList(qq interface{}) (map[string]interface{}, error) {
+	bot, err := Bot.BotSingle(qq)
+	if err != nil {
+		return nil, err
+	} else {
+		get := map[string]interface{}{
+			"sessionKey": bot.SessionKey,
+		}
+		ret, err := Net.Get(bot.URL+"/groupList", get, nil, nil)
+		if err != nil {
+			Log.Crrs(err, tuuz.FUNCTION_ALL())
+		} else {
+			return Jsong.JObject(ret)
+		}
+		return nil, err
+	}
+}

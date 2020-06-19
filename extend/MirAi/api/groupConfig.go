41c92ea46183f15a12619601425076a8083c1659
Tuuz <2539@aerofsx.com>
2020-06-19 17:43:51 +0800

    Tuuz

---
+++ extend/MirAi/api/groupConfig.go
@@ -0,0 +1,14 @@
+package api
+
+type Config struct {
+	name              string
+	announcement      string
+	confesstalk       bool
+	allowMemberInvite bool
+	autoApprove       bool
+	anonymousChat     bool
+}
+
+func groupConfig(qq, group_id interface{}, config Config) (map[string]interface{}, error) {
+
+}

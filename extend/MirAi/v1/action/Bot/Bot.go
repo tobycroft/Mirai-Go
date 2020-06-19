025b69aa312be83dbbb865401b29fd64ce74b891
Tuuz <2539@aerofsx.com>
2020-06-19 14:56:43 +0800

    Tuuz

--- extend/MirAi/v1/action/Bot/Bot.go
+++ extend/MirAi/v1/action/Bot/Bot.go
@@ -1,6 +1,7 @@
 package Bot
 
 import (
+	"errors"
 	"main.go/extend/MirAi/v1/model/BotModel"
 	"main.go/tuuz/Calc"
 )
@@ -12,21 +13,21 @@ type Robot struct {
 	SessionKey string
 }
 
-func BotSingle(qq interface{}) (Robot, bool) {
+func BotSingle(qq interface{}) (Robot, error) {
 	bot := BotModel.Api_find(qq)
 	if len(bot) < 1 {
-		return Robot{}, false
+		return Robot{}, errors.New("未找到账号，可能机器人已经过期")
 	}
 	url := Calc.Any2String(bot["url"])
 	authKey := Calc.Any2String(bot["authKey"])
 	sessionKey := Calc.Any2String(bot["sessionKey"])
-	return Robot{URL: url, AuthKey: authKey, SessionKey: sessionKey, QQ: Calc.Any2String(qq)}, true
+	return Robot{URL: url, AuthKey: authKey, SessionKey: sessionKey, QQ: Calc.Any2String(qq)}, nil
 }
 
-func BotAll() ([]Robot, bool) {
+func BotAll() ([]Robot, error) {
 	bots := BotModel.Api_select()
 	if len(bots) < 1 {
-		return nil, false
+		return nil, errors.New("未找到账号，可能机器人已经过期")
 	}
 	Bots := []Robot{}
 	for _, bot := range bots {
@@ -36,5 +37,5 @@ func BotAll() ([]Robot, bool) {
 		sessionKey := Calc.Any2String(bot["sessionKey"])
 		Bots = append(Bots, Robot{URL: url, AuthKey: authKey, SessionKey: sessionKey, QQ: Calc.Any2String(qq)})
 	}
-	return Bots, true
+	return Bots, nil
 }

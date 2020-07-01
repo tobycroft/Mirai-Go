293a4bdc00436ffa23e3dee56637fd4e9d3e9fa8
TobyCroft <silverhawk@foxmail.com>
2020-07-02 00:00:01 +0800

    Tuuz

---
+++ extend/MirAi/api/newFriendRequestEvent.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"main.go/extend/MirAi/v1/action/Bot"
+	"main.go/tuuz"
+	"main.go/tuuz/Jsong"
+	"main.go/tuuz/Log"
+	"main.go/tuuz/Net"
+)
+
+func NewFriendRequestEvent(qq, eventId, fromId, groupId, operate, message interface{}) (map[string]interface{}, error) {
+	bot, err := Bot.BotSingle(qq)
+	if err != nil {
+		return nil, err
+	} else {
+		post := map[string]interface{}{
+			"sessionKey": bot.SessionKey,
+			"eventId":    eventId,
+			"fromId":     fromId,
+			"groupId":    groupId,
+			"operate":    operate,
+			"message":    message,
+		}
+		ret, err := Net.Postraw(bot.URL+"/resp/newFriendRequestEvent", nil, post, nil, nil)
+		if err != nil {
+			Log.Crrs(err, tuuz.FUNCTION_ALL())
+			return nil, err
+		} else {
+			return Jsong.JObject(ret)
+		}
+	}
+}

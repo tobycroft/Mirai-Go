9512709238054fe8f8de921bfaac0df1583d7bf7
TobyCroft <silverhawk@foxmail.com>
2020-07-02 23:04:40 +0800

    Tuuz

--- extend/MirAi/v1/action/Request/BotInvitedJoinGroupRequest.go
+++ extend/MirAi/v1/action/Request/BotInvitedJoinGroupRequest.go
@@ -1,6 +1,7 @@
 package Request
 
 import (
+	"main.go/app/v1/api/action/BotAction"
 	"main.go/app/v1/api/model/BotAllowGroupModel"
 	"main.go/extend/MirAi/api"
 	"main.go/extend/MirAi/v1/model/GroupRequestModel"
@@ -8,11 +9,14 @@ import (
 
 func BotInvitedJoinGroupRequest(qq string, json map[string]interface{}) {
 	GroupRequestModel.Api_insert(qq, json["eventId"], json["message"], json["fromId"], json["groupId"], json["groupName"], json["nick"])
-	allow := BotAllowGroupModel.Api_find(qq, json["groupId"])
-	if len(allow) > 0 {
-		api.MemberJoinRequestEvent(qq, json["eventId"], json["fromId"], json["groupId"], 0, "")
+	if BotAction.App_LockGroup(qq) {
+		allow := BotAllowGroupModel.Api_find(qq, json["groupId"])
+		if len(allow) > 0 {
+			api.MemberJoinRequestEvent(qq, json["eventId"], json["fromId"], json["groupId"], 0, "")
+		} else {
+			api.MemberJoinRequestEvent(qq, json["eventId"], json["fromId"], json["groupId"], 1, "机器人主不允许加入本群")
+		}
 	} else {
-		api.MemberJoinRequestEvent(qq, json["eventId"], json["fromId"], json["groupId"], 1, "机器人主不允许加入本群")
+		api.MemberJoinRequestEvent(qq, json["eventId"], json["fromId"], json["groupId"], 0, "")
 	}
-
 }

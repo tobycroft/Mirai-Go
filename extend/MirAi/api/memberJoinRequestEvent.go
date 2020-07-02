5badc81ba47754c5a77bca15041e5b54affa5621
TobyCroft <silverhawk@foxmail.com>
2020-07-02 22:06:00 +0800

    Tuuz

--- extend/MirAi/api/memberJoinRequestEvent.go
+++ extend/MirAi/api/memberJoinRequestEvent.go
@@ -42,24 +42,5 @@ func AllowInviteGroup(qq, eventId, fromId, groupId, operate, message interface{}
 		0	同意邀请
 		1	拒绝邀请
 	*/
-	bot, err := Bot.BotSingle(qq)
-	if err != nil {
-		return nil, err
-	} else {
-		post := map[string]interface{}{
-			"sessionKey": bot.SessionKey,
-			"eventId":    eventId,
-			"fromId":     fromId,
-			"groupId":    groupId,
-			"operate":    operate,
-			"message":    message,
-		}
-		ret, err := Net.Postraw(bot.URL+"/resp/memberJoinRequestEvent", nil, post, nil, nil)
-		if err != nil {
-			Log.Crrs(err, tuuz.FUNCTION_ALL())
-			return nil, err
-		} else {
-			return Jsong.JObject(ret)
-		}
-	}
+	return AllowJoinIn(qq, eventId, fromId, groupId, operate, message)
 }

c1fcc435bf99bd600cfd987ac86e446d50f5093b
TobyCroft <silverhawk@foxmail.com>
2020-07-02 23:03:48 +0800

    Tuuz

--- app/v1/api/action/BotAction/BotAction.go
+++ app/v1/api/action/BotAction/BotAction.go
@@ -2,12 +2,16 @@ package BotAction
 
 import "main.go/app/v1/api/model/BotFunctionModel"
 
-func App_LockGroup(qq interface{}) int64 {
+func App_LockGroup(qq interface{}) bool {
 	botfunction := BotFunctionModel.Api_find(qq)
 	if len(botfunction) > 0 {
-		return botfunction["lock_group"].(int64)
+		if botfunction["lock_group"].(int64) == 1 {
+			return true
+		} else {
+			return false
+		}
 	} else {
 		BotFunctionModel.Api_insert(qq)
-		return 1
+		return true
 	}
 }

98c89e07db9606df329c1a7d4542fee285bd5d82
Tuuz <2539@aerofsx.com>
2020-07-03 15:08:40 +0800

    Tuuz

--- extend/MirAi/api/quit.go
+++ extend/MirAi/api/quit.go
@@ -8,6 +8,27 @@ import (
 	"main.go/tuuz/Net"
 )
 
+/*
+退出群聊
+使用此方法使Bot退出群聊
+
+[POST] /quit
+请求:
+{
+    "sessionKey": "YourSessionKey",
+    "target": 123456789,
+}
+名字	可选	类型	举例	说明
+sessionKey	false	String	"YourSessionKey"	你的session key
+target	false	Long	123456789	退出的群号
+响应
+响应: 返回统一状态码
+{
+    "code": 0,
+    "msg": "success"
+}
+bot为该群群主时退出失败并返回code 10(无操作权限)
+*/
 func Quit(qq, group_id interface{}) (map[string]interface{}, error) {
 	bot, err := Bot.BotSingle(qq)
 	if err != nil {
